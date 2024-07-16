2cea18c2c33df57c3a1b7b634c1aeb42863e404a
Aisha <a.maurya.1403@gmail.com>
2024-07-16 19:27:07 +0530

    updated changes

---
+++ src/products.js
@@ -0,0 +1,61 @@
+import product1 from './assets/headphone.jpg';
+import product2 from './assets/keyboard.webp';
+import product3 from './assets/lamp.webp';
+import product4 from './assets/laptop.jpg';
+import product5 from './assets/mobile.webp';
+import product6 from './assets/mouse.webp';
+import product7 from './assets/tablet.webp';
+import product8 from './assets/wirelessmouse.jpg';
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "Headphones",
+        price: 30,
+        productImage: product1,
+    },
+
+    {
+        id:2,
+        productName: "Keyboard",
+        price: 28,
+        productImage: product2,
+    },
+    {
+        id:3,
+        productName: "Lamp",
+        price: 15,
+        productImage: product3,
+    },
+
+    {
+        id:4,
+        productName: "Laptop",
+        price: 999,
+        productImage: product4,
+    },
+    {
+        id:5,
+        productName: "Mobile",
+        price: 780,
+        productImage: product5,
+    },
+    {
+        id:6,
+        productName: "Mouse",
+        price: 30,
+        productImage: product6,
+    },
+    {
+        id:7,
+        productName: "Tablet",
+        price: 600,
+        productImage: product7,
+    },
+    {
+        id:8,
+        productName: "WirelessMouse",
+        price: 30,
+        productImage: product8,
+    },
+]
